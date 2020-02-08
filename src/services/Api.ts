a68ff31bded96ec44ad68b57d6f29ceaaa55448a
Maciej Borzymowski <maciej.borzymowski@nextima.com>
2020-02-08 13:50:12 +0100

    env update (I know it should't be there)

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const AppAPI = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const fetchSongs = (search: string) => AppAPI.get(`/ra/songs.json?pattern=${search}`);
